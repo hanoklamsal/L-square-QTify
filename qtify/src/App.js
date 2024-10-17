051f0af6deff0c5058a51bd518aa3014047688f9
Hanok Lamsal <hanoklamsal@Hanoks-MacBook-Air.local>
2024-10-17 13:32:45 +0530

    module 2 implemented

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
+import MyCard from './components/Card/MyCard';
+import Section from './components/Section/Section';
 
 function App() {
   return (
-    <div>
+    <div className='app'>
       <Navbar/>
       <Hero/>
+      <Section/>
     </div>
   );
 }
