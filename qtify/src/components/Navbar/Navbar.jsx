0535d9ab43969ca42e83287bc075068db28ecf3f
Hanok Lamsal <hanoklamsal@Hanoks-MacBook-Air.local>
2024-10-23 11:37:59 +0530

    fix

--- qtify/src/components/Navbar/Navbar.jsx
+++ qtify/src/components/Navbar/Navbar.jsx
@@ -10,7 +10,7 @@ export default function Navbar(){
         <>
             <nav className={styles.navbar}>
                 <div className={styles.logoDiv}><img  src={LogoImage} alt="logo" width={67}/></div>
-                 <Search search={"Search a song of your choice"} placeholder={"search"}/>
+                 <Search search={"Search a song of your choice"} placeholder="Search a song of your choice"/>
                 <Button children="Give Feedback"/>
             </nav>
         </>
